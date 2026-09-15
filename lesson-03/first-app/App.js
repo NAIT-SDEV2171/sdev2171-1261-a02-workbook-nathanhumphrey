c54afd7324a1f92d4cf72d27f9d68ca14713da01
Nathan Humphrey <NathanH@nait.ca>
2026-09-15 11:57:11 -0600

    Update first-app with lesson 5 content

--- lesson-03/first-app/App.js
+++ lesson-03/first-app/App.js
@@ -1,22 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Image } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeArea}>
       <StatusBar style="dark" />
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Some more text!</Text>
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <View>
+          <Image style={styles.appImage} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+          <Text style={styles.title}>Application Name</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.card.title}>Card Title</Text>
+          <Text style={styles.card.body}>Card body text</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.card.title}>Card Title</Text>
+          <Text style={styles.card.body}>Card body text</Text>
+          <TextInput
+            defaultValue="A new text input component."
+            multiline
+            style={styles.textInput}
+            placeholder="Type whatever you like here"
+            placeholderTextColor="grey"></TextInput>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    backgroundColor: '#f00',
+    backgroundColor: 'rgb(178, 248, 253)',
     alignItems: 'center',
-    // justifyContent: 'center',
   },
+  scrollContent: {
+    padding: 8,
+  },
+  appImage: {
+    height: 80,
+    width: 80,
+    alignSelf: 'center',
+    marginBottom: 16,
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  card: {
+    backgroundColor: 'rgb(220, 245, 247)',
+    borderColor: 'rgb(110, 155, 158)',
+    borderRadius: 4,
+    borderWidth: 2,
+    marginBottom: 16,
+    padding: 8,
+    title: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      marginBottom: 8,
+    },
+    body: {
+      marginBottom: 8,
+    }
+  },
+  textInput: {
+    minHeight: 80,
+    borderWidth: 1,
+    borderColor: 'rgb(110, 155, 158)',
+    borderRadius: 4,
+    padding: 12,
+    fontSize: 14,
+    lineHeight: 20,
+    backgroundColor: '#fffdfb',
+    textAlignVertical: 'top',
+  }
 });
