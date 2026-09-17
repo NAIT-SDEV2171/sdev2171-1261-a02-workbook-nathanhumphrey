d68e512dafd1c770516faf5c8440a441245b9424
Nathan Humphrey <NathanH@nait.ca>
2026-09-17 11:38:52 -0600

    Lesson 6 first-app multiple nav options

--- lesson-03/first-app/src/app/details.jsx
+++ lesson-03/first-app/src/app/details.jsx
@@ -1,39 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, TextInput, Image, Button, Pressable } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Image } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Link, useRouter } from "expo-router";
 
-export default function Home() {
-
-  const router = useRouter();
-
+export default function Details() {
   return (
     <SafeAreaView style={styles.safeArea}>
       <StatusBar style="dark" />
       <ScrollView contentContainerStyle={styles.scrollContent}>
         <View>
           <Image style={styles.appImage} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
-          <Text style={styles.title}>Application Name</Text>
+          <Text style={styles.title}>Application Name - Details Page</Text>
         </View>
         <View style={styles.card}>
           <Text style={styles.card.title}>Card Title</Text>
           <Text style={styles.card.body}>Card body text</Text>
         </View>
-        <View style={styles.card}>
-          <Text style={styles.card.title}>Card Title</Text>
-          <Text style={styles.card.body}>Card body text</Text>
-          <TextInput
-            defaultValue="A new text input component."
-            multiline
-            style={styles.textInput}
-            placeholder="Type whatever you like here"
-            placeholderTextColor="grey"></TextInput>
-        </View>
-        <Button style={{backgroundColor: "#F00"}} title="Go to Details" onPress={() => router.navigate('/details')} />
-        <Link style={{backgroundColor: "#F00"}} href="/details">Go to Details</Link>
-        <Pressable style={{backgroundColor: "#F00"}} onPress={() => router.navigate('/details')}>
-          <Text>Go to Details</Text>
-        </Pressable>
       </ScrollView>
     </SafeAreaView>
   );
